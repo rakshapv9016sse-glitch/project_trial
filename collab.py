54945256da43764d09de412c2de33c56948004c6
raksh <rakshapv9016.sse@saveetha.com>
2026-03-23 12:59:56 +0530

    initial commit

---
+++ collab.py
@@ -0,0 +1,10 @@
+def student_result(name,marks):
+    if marks >= 50:
+        print(name,"pass")
+    else:
+        print(name,"fail")
+student_result("raksha",50)
+student_result("gloria",60)
+student_result("alex",99)
+student_result("luke",25)
+student_result("manny",35)
