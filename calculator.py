f29d3c7aea073b8bcdc776b16f587c0505de46d1
raksh <rakshapv9016.sse@saveetha.com>
2026-03-23 10:02:17 +0530

    inital commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def modulus(a,b):
+    return a%b
+print("add:",add(26,7))
+print("sub:",subtract(26,7))
+print("multiply:",multiply(26,7))
+print("divide:",divide(26,7))
+print("modulus:",modulus(26,7))
